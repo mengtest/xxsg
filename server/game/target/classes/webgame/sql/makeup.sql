905d4688f29bffe6e94d0d674efadbbf90328d8c
parasol_qian <parasol_qian@qq.com>
2020-09-23 09:53:06 +0800

    22

--- server/game/target/classes/webgame/sql/makeup.sql
+++
@@ -1,6 +0,0 @@
-REPLACE INTO tb_constant(`key`,`value`) VALUES ('version','1.1380');
-ALTER TABLE `tb_single_ins` ADD `actPlayTime` TEXT NOT NULL ;
-UPDATE `tb_single_ins` SET `actPlayTime` = '{}';
-ALTER TABLE `tb_task` ADD `findRewardMax` TEXT NOT NULL ;
-UPDATE `tb_task` SET `findRewardMax` = '{}';
-ALTER TABLE `tb_welfare` ADD `backGiftId` INT(11) NOT NULL DEFAULT '0';
